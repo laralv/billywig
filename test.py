76b8c5df268cdda0ce6cc337a55ce948610d04c5
Lars Jynge Alvik <laralv@arkivverket.no>
2017-02-08 12:43:05 +0100

    Getting SHA-checks to work, small

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import ebcdic
 import yaml
 import re
@@ -11,7 +10,7 @@ import hashlib
 # Setting up argparse
 parser = argparse.ArgumentParser(description='EBCDIC to-human-readable script')
 parser.add_argument('--configfile', type=str, help='Path to config')
-parser.add_argument('--outputfile', type=str, help='Output path')
+parser.add_argument('--output', type=str, help='Output path')
 sys_args = parser.parse_args()
 if sys_args.configfile:
     config = yaml.safe_load(open(sys_args.configfile).read())
@@ -22,17 +21,16 @@ fileList = glob.glob('*')
 fileMatchList = []
 for fileHash in fileList:
     if hashlib.sha256(open(fileHash, 'rb').read()).hexdigest() in config['applies_to']:
-        print(fileHash)
         fileMatchList.append(fileHash)
-
+print(fileMatchList)
 # Open file from config
-## trenger buffret innlesning
-fileIn = open(config['fileName']).read().decode(config['input_charset'])
-
-recordsList = map(''.join, zip(*[iter(fileIn)]*config['post_length']))
+## Trenger buffret innlesning
+## Nå leser den hele filen inn i minnet før den splitter opp.
+recordsList = []
+for fileMatch in fileMatchList:
+    fileIn = open(fileMatch, 'rb').read().decode(config['input_charset'])
+    recordsList.extend(map(''.join, zip(*[iter(fileIn)]*config['post_length'])))
 fileIn = ''
-
-## Skriv inn sjekk mot SHA
 
 for record in recordsList:
     record_json = {}
