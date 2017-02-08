e4aa80d905c87edd41bb06e58b498fb28e14e4f3
Lars Jynge Alvik <laralv@arkivverket.no>
2017-02-08 13:53:57 +0100

    Added hash-buffering

--- test.py
+++ test.py
@@ -16,22 +16,33 @@ if sys_args.configfile:
     config = yaml.safe_load(open(sys_args.configfile).read())
 ## Her trengs exception hvis config ikke eksisterer
 
-# Checking hashes against files in folder. 
+# Checking hashes against files in folder.
+# inputfiles are buffered into 64kb slices.
 fileList = glob.glob('*')
 fileMatchList = []
 for fileHash in fileList:
-    if hashlib.sha256(open(fileHash, 'rb').read()).hexdigest() in config['applies_to']:
+    hashFileSha256 = hashlib.sha256()
+    hashBufferSize = 65536
+    with open(fileHash, 'rb') as f:
+        while True:
+            hashData = f.read(hashBufferSize)
+            if not hashData:
+                break
+            hashFileSha256.update(hashData)
+    if hashFileSha256.hexdigest() in config['applies_to']:
         fileMatchList.append(fileHash)
 print(fileMatchList)
 # Open file from config
 ## Trenger buffret innlesning
 ## Nå leser den hele filen inn i minnet før den splitter opp.
+## Men kan vi buffre ebcdic når vi skal gjøre om til utf8?
 recordsList = []
 for fileMatch in fileMatchList:
     fileIn = open(fileMatch, 'rb').read().decode(config['input_charset'])
     recordsList.extend(map(''.join, zip(*[iter(fileIn)]*config['post_length'])))
 fileIn = ''
 
+record_jsonList = []
 for record in recordsList:
     record_json = {}
     for column in config['columns']:
@@ -47,5 +58,6 @@ for record in recordsList:
                     record_json[column['columnName']] = record[column['columnStart']:column['columnEnd']]
             continue
         record_json[column['columnName']] = record[column['columnStart']:column['columnEnd']]
-    print(record_json)
+    record_jsonList.append(record_json)
+print(len(record_jsonList))
 
